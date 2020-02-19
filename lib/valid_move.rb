3e6589bc25c5560f13a26d17a208ee9d1645ef2d
Mark Stabler <mark@stablerwritingservices.com>
2020-02-19 19:52:47 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,11 @@
 # code your #valid_move? method here
-
+def valid_move?(index)
+	if 0 <= index <= 8
+		!position_taken?(board, index)
+	else
+		false
+	end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
